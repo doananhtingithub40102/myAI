3137cd6ec01446a67942f0c92a2d20cc85a30245
doananhtingithub40102 <doananhtin2001st@gmail.com>
2022-08-29 19:45:58 +0700

    edit BFS.py file

--- Algorithms/BFS.py
+++ Algorithms/BFS.py
@@ -8,7 +8,7 @@ import sys
 import os
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__)) # Thư mục cha của file BFS.py
-sys.path.append(os.path.dirname(SCRIPT_DIR)) # thêm "d:\AI" vào danh sách đường dẫn
+sys.path.append(os.path.dirname(SCRIPT_DIR)) # thêm "d:\myAI" vào danh sách đường dẫn
 
 from DataStructure.QueueUsingArray import QueueUsingArray
 
@@ -18,10 +18,11 @@ class Graph:
 
     def addEdge(self, u, v):
         self.graph[u].append(v)
+        self.graph[v].append(u)
 
     def BFS(self, s):
         visited = [False] * (max(self.graph) + 1)
-        queue = QueueUsingArray(max(self.graph))
+        queue = QueueUsingArray(10)
 
         # Thêm s (nút start) vào Queue trước tiên và đánh dấu đã duyệt qua 
         queue.enQueue(s)
@@ -40,37 +41,12 @@ class Graph:
 
 if __name__ == "__main__":
     g = Graph()
-    g.addEdge(1, 2)
+    g.addEdge(0, 1)
+    g.addEdge(0, 2)
     g.addEdge(1, 3)
-    g.addEdge(1, 11)
-    g.addEdge(2, 1)
+    g.addEdge(1, 4)
     g.addEdge(2, 4)
-    g.addEdge(2, 6)
-    g.addEdge(3, 1)
-    g.addEdge(3, 4)
-    g.addEdge(4, 2)
-    g.addEdge(4, 3)
-    g.addEdge(4, 6)
-    g.addEdge(5, 9)
-    g.addEdge(5, 10)
-    g.addEdge(6, 2)
-    g.addEdge(6, 4)
-    g.addEdge(6, 7)
-    g.addEdge(6, 8)
-    g.addEdge(7, 6)
-    g.addEdge(8, 6)
-    g.addEdge(8, 10)
-    g.addEdge(9, 5)
-    g.addEdge(9, 13)
-    g.addEdge(10, 5)
-    g.addEdge(10, 8)
-    g.addEdge(11, 1)
-    g.addEdge(11, 12)
-    g.addEdge(11, 13)
-    g.addEdge(12, 11)
-    g.addEdge(12, 13)
-    g.addEdge(13, 9)
-    g.addEdge(13, 11)
-    g.addEdge(13, 12)
+    g.addEdge(2, 5)
+    g.addEdge(4, 5)
 
-    g.BFS(1)
+    g.BFS(0)
